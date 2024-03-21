a47cd7115c8e4cbd0ddb1b8d4a11ef7601de1e86
Akeim Sutherland <akeim.sutherland@sitebix.com>
2024-03-21 15:55:43 -0400

    updated credentials

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "6858733203:AAE2wvWqaIevHBzHQSB-rb6fGE0vy4b56r8"
-bot_user_name = "Jermaine94Bot"
-URL = "https://mybot-az5r.onrender.com"
+bot_user_name = "@Jermaine94Bot"
+URL = "https://mybot-production-9ef9.up.railway.app/"
